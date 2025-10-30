cecb7100ed42827fb876676a62a5ccabf6229450
Ebee1205 <spiverain02@gmail.com>
2025-10-31 00:05:22 +0900

    build: v0.1.0

--- vite.config.mjs
+++ vite.config.mjs
@@ -11,7 +11,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/Moodimal/' : '',
+  base: process.env.NODE_ENV === 'production' ? '/moodimal-FE/' : '',
   build: {
     outDir: 'docs',
     emptyOutDir: true,
@@ -31,6 +31,7 @@ export default defineConfig({
     }),
     Components(),
   ],
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
